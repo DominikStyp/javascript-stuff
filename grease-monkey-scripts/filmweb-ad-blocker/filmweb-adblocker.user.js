3dfbacaf8afe9a7193cbdc2077fbc9eb1e06d911
DominikStyp <dominik.stypula1@gmail.com>
2017-11-09 00:54:25 +0100

    v1.1.4: removed clone in favor of document.querySelector.bind(document); which perfectly clones native function.

--- grease-monkey-scripts/filmweb-ad-blocker/filmweb-adblocker.user.js
+++ grease-monkey-scripts/filmweb-ad-blocker/filmweb-adblocker.user.js
@@ -12,37 +12,17 @@
 // @include         https://*.filmweb.pl/*
 // @match           http://*.filmweb.pl/*
 // @match           https://*.filmweb.pl/*
-// @version 		1.1.3
+// @version 		1.1.4
 // @grant      		none
 // ==/UserScript==
 
 (function() {
     'use strict';
-     var cloneObj = function (obj) {
-         var temp;
-         if (obj === null || typeof(obj) !== 'object' || 'isActiveClone' in obj){
-             return obj;
-         }
-         if (obj instanceof Date) {
-             temp = new obj.constructor(); //or new Date(obj);
-         }
-         else {
-             temp = obj.constructor();
-         }
-         for (var key in obj) {
-             if (Object.prototype.hasOwnProperty.call(obj, key)) {
-                 obj['isActiveClone'] = null;
-                 temp[key] = clone(obj[key]);
-                 delete obj['isActiveClone'];
-             }
-         }
-         return temp;
-    };
-
     setTimeout(function(){
         window.hasAdblock = false;
         window.waitingModule.runWhenReady = function() { return false; };
-        var qs = cloneObj(document.querySelector);
+		// CLONE querySelector() function so overriding native function won't affect qs()
+        var qs = document.querySelector.bind(document);
         document.querySelector = function(param){
             if(param.indexOf(".filmCastBox") !== -1){
                 return function(){
@@ -52,5 +32,5 @@
             return qs(param);
        };
        console.log("Filmweb Adblocker version: " + GM_info.script.version);
-    }, 5);
+    }, 10);
 })();
